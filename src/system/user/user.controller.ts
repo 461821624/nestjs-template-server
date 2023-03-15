395f2b0691f3bda7ff65c3f74cd249ebfb89d121
yanji <461821624@qq.com>
2023-03-15 17:10:46 +0800

    Add files via upload

---
+++ src/system/user/user.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+  ClassSerializerInterceptor,
+  UseInterceptors,
+  Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { ResultData } from '../../common/dto/result-data.dto';
+import { ApiResult } from '../../common/decorator/api-result.decorator';
+import { FindUserDao } from './dao/find-user.dao';
+import { PageQueryDto } from '../../common/dto/page-query.dto';
+@ApiTags('User')
+@ApiBearerAuth()
+@Controller('user')
+@ApiExtraModels(ResultData, FindUserDao, PageQueryDto)
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+  @ApiResult(FindUserDao)
+  @Get('profile')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UseGuards(AuthGuard('jwt'))
+  async getUserProfile(@Request() req): Promise<ResultData> {
+    return await this.userService.getUserProfile(req.user.userId);
+  }
+  //
+  @Get('list')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiResult(FindUserDao, true, true)
+  findUser(@Query() pageQueryDto: PageQueryDto): Promise<ResultData> {
+    const { pageNo: page, pageSize: limit, ...params } = pageQueryDto;
+    return this.userService.findUser(params, { page, limit });
+  }
+  //
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+  //
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
+}
---
+++ src/system/user/user.service.ts
@@ -0,0 +1,102 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import Redis from 'ioredis';
+import * as config from 'config';
+import { ResultData } from '../../common/dto/result-data.dto';
+import { paginate, IPaginationOptions } from 'nestjs-typeorm-paginate';
+import * as bcrypt from 'bcrypt';
+import { ApiHttpCode } from '../../common/enums/api-http-code';
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    @InjectRedis() private readonly client: Redis,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+
+  findByUsername(username): Promise<User> {
+    return this.usersRepository.findOne({
+      where: {
+        username: username,
+      },
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  async getUserProfile(id: any) {
+    const result = await this.client.get(
+      `${config.get('server.name')}:user_id`,
+    );
+    if (!result) {
+      const user = await this.usersRepository.findOneBy({
+        id: id,
+      });
+      await this.client.set(
+        `${config.get('server.name')}:user_id`,
+        JSON.stringify(user),
+      );
+      return ResultData.ok(user);
+    }
+
+    return ResultData.ok(JSON.parse(result));
+  }
+
+  async findUser(params: any, option: IPaginationOptions) {
+    const { keyword, sortBy, descending } = params;
+    const search = keyword || '';
+    const sort = sortBy || 'create_time';
+    const des = descending || 'ASC';
+    const queryBuilder = this.usersRepository.createQueryBuilder('user');
+    if (search) {
+      queryBuilder
+        .where('user.name LIKE :name', { name: '%' + search + '%' })
+        .andWhere('user.deleted = :deleted', { deleted: 0 })
+        .orderBy(`user.${sort}`, des); // Or whatever you need to do
+    } else {
+      queryBuilder
+        .orderBy(`user.${sort}`, des)
+        .where('user.deleted = :deleted', { deleted: 0 }); // Or whatever you need to do
+    }
+    const promise = await paginate<User>(queryBuilder, option);
+    return ResultData.ok({ list: promise.items, ...promise.meta });
+  }
+
+  async findByUser(username: string, password: string) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        username: username,
+      },
+    });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
