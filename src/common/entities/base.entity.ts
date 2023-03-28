a982697a12dd99c4576f65b2d6e687bf03dc8a94
songshunshun <461821624@qq.com>
2023-03-28 13:46:07 +0800

    day.js替换moment.js

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,13 +1,11 @@
-import { Exclude, Transform, Type } from 'class-transformer';
+import { Exclude, Type } from 'class-transformer';
 import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-import * as moment from 'moment';
-import { Moment } from 'moment';
+
 export class BaseEntity {
   @Column({ type: 'varchar', length: 64 })
   creator: string;
   @CreateDateColumn({ type: 'datetime' })
   @Type(() => Date)
-  @Transform(({ value }) => moment(value).format('YYYY-MM-DD HH:mm:ss'))
   create_time: Date;
   @Column({ type: 'varchar', length: 64 })
   updater: string;
